15e5ffd49c60091aecf3c9cc7257da5096d9bded
LucasEVAvila <Lucasvavila@gmail.com>
2025-11-26 19:42:11 -0300

    teste

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,31 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
 use App\Http\Controllers\ContatoController;
-use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 
-// Rotas Públicas
+
+// 1. Rotas Públicas 
 Route::get('/', [PrincipalController::class, 'home'])->name('index');
 Route::get('/sobre-nos', [SobreNosController::class, 'aboutUs'])->name('about');
 Route::get('/contato', [ContatoController::class, 'contact'])->name('contact');
 Route::post('/contato', [ContatoController::class, 'salvar'])->name('contact.salvar');
 
-// Grupo de Rotas do Admin
-Route::prefix('admin')->group(function () {
-    Route::get('/login', [LoginController::class, 'index'])->name('admin.login');
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-
+// 2. Rotas Protegidas 
+Route::middleware(['auth', 'verified'])->group(function () {
+    
+    
+    // Rotas do Profile do Breeze
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+  
+   
 });
+
+// 3. Rotas de Autenticação do Breeze (Login, Register, etc.)
+require __DIR__.'/auth.php';troy'])->name('profile.destroy');
